5c0b9e4a83a84be9fb04271899d8aec7ca3a7a07
Dumas-Lavoie <goglu1@live.ca>
2016-07-29 18:04:20 -0400

    Ajout d'une zone de texte
    Ajout d’une zone de texte et alignement de celle-ci dans la fenêtre.
    Ajout d’un conteneur.

--- src/Fenetre1.java
+++ src/Fenetre1.java
@@ -1,20 +1,33 @@
 import javax.swing.JFrame;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Fenetre1 extends JFrame {
-
+	private JPanel container = new JPanel();
+	private JTextField texte1 = new JTextField("Entrer votre texte ici.");
+	
 	public Fenetre1(){
-		
 		this.setTitle("PVO");
 		this.setSize(1100, 700);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.isResizable();
-	
+		
+		container.setBackground(Color.WHITE);
 		JPanel pan1 = new JPanel();
-		pan1.setBackground(Color.WHITE);
-		this.setContentPane(pan1);
+		
+		Font police = new Font("Comic Sans MS", Font.BOLD, 16);
+		texte1.setFont(police);
+		texte1.setForeground(Color.BLACK);
+		pan1.add(texte1);
+		container.add(pan1, BorderLayout.NORTH);
+		
+		this.setContentPane(container);
 			
 		this.setVisible(true);
 
